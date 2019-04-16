a0aa0252e4de0ca2866a4b35e6a3945f24d52481
Brett Neese <brett@neese.rocks>
2019-04-15 23:16:17 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # k8s-to-ssm
+
 A simple CLI tool to upload K8S secrets to AWS SSM
+
+`k8sssm ${ssm_path} ${json/yaml data}`
+
+1. `npm install -g k8s-to-ssm`
+2. Examples:
+
+`kubectl get --export secrets -o json | k8s2ssm /ssmpath -`
+`cat secrets.yaml | ./index.js k8s2ssm /ssmpath -`
---
+++ index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const AWS = require("aws-sdk");
+const YAML = require("yaml");
+const ssm = new AWS.SSM({ apiVersion: "2014-11-06" });
+
+let stdin = process.openStdin();
+let data = "";
+
+let destinationPath = process.argv[2];
+
+if (destinationPath.slice(-1) == "/") {
+  destinationPath = destinationPath.slice(0, -1);
+}
+
+async function addParam(key, value, type, allowUpdate) {
+  let param = {
+    Name: destinationPath + "/" + key,
+    Type: type || "SecureString",
+    Value: Buffer.from(value, "base64").toString(),
+    Overwrite: (allowUpdate && allowUpdate == "false") || true
+  };
+
+  ssm.putParameter(param, function(err, data) {
+    if (err) {
+      console.log("Error adding parameter: " + param.Name);
+      console.log(err, err.stack);
+    } else {
+      console.log("Added/updated parameter: " + param.Name);
+    }
+  });
+}
+
+stdin.on("data", function(chunk) {
+  data += chunk;
+});
+
+stdin.on("end", function() {
+  try {
+    let items = JSON.parse(data).items;
+  } catch (e) {
+    // assume we've got yaml, not json
+    items = YAML.parse(data).items;
+  }
+
+  items.forEach(function(item) {
+    if (item.type != "kubernetes.io/service-account-token") {
+      Object.keys(item.data).forEach(async function(key) {
+        addParam(
+          item.metadata.name + "/" + key,
+          item.data[key],
+          item.metadata.annotations["awsParam.Type"],
+          item.metadata.annotations["awsParam.allowUpdate"]
+        );
+      });
+    }
+  });
+});
